9e7fbb86258d7eede5d6d7d8e0a1f3f35cb3733e
Ahnaf Jamil <ahnaf.jamil52@gmail.com>
2025-11-25 09:25:16 +0600

    Get Started hover behaviour update on Industry Page

--- src/pages/Onboarding/IndustryDashboardPage.tsx
+++ src/pages/Onboarding/IndustryDashboardPage.tsx
@@ -106,7 +106,7 @@ const IndustryDashboardPage = () => {
 
               {industry.available ? (
                 <Button
-                  className="w-full group-hover:bg-accent group-hover:text-accent-foreground transition-colors"
+                  className="w-full hover:bg-cyan-700 group-hover:text-accent-foreground transition-colors"
                   onClick={() => handleIndustrySelect(industry.value)}
                 >
                   Get Started
